c58973084729bb677e57d4ead455f718c0d72472
lihzoh7 <lihzoh7@gmail.com>
2026-09-15 19:04:09 +0200

    Modify callback URLs in payment payload
    Updated callback URLs in payload for payment processing.

--- index.js
+++ index.js
@@ -23,25 +23,24 @@ app.post('/deposit/1voucher', async (req, res) => {
 const authHeader = "Basic " + Buffer.from("IamLizo:1Aml!zo#123").toString("base64");
 
     const payload = {
-      merchantBranchProductNumber: "JQVSND",
-      totalCostInCents: parseInt(amountInCents, 10),
-      transactionDescription: `1Voucher Wallet Deposit - ${userId}`,
-      merchantReferenceNumber: `DEP-${Date.now()}`,
-      userHostAddress: "127.0.0.1",
-      resultRedirectUrl: "https://muustandibackend.onrender.com/wallet-success",
-      callbackUrl: "https://muustandibackend.onrender.com/api/1voucher/callback",
-      cartItems: null,
-      paymentChannels: [
-        {
-          channelName: "OneVoucher",
-          settings: {
-            pin: voucherPin
-          }
-        }
-      ],
-      merchantClientProfile: "PMV00003"
-    };
-
+  merchantBranchProductNumber: "JQVSND",
+  totalCostInCents: parseInt(amountInCents, 10),
+  transactionDescription: `1Voucher Wallet Deposit - ${userId}`,
+  merchantReferenceNumber: `DEP-${Date.now()}`,
+  userHostAddress: "127.0.0.1",
+  resultCallbackUrl: "https://muustandibackend.onrender.com/wallet-success",
+  notifyCallbackUrl: "https://muustandibackend.onrender.com/api/1voucher/callback",
+  cartItems: null,
+  paymentChannels: [
+    {
+      channelName: "OneVoucher",
+      settings: {
+        pin: voucherPin
+      }
+    }
+  ],
+  merchantClientProfile: "PMV00003"
+};
     const response = await fetch("https://paym8online.com/PaymentsService/api/V1/ecommerce/SubmitPaymentRequest", {
       method: "POST",
       headers: {
